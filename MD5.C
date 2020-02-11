8ea92d18bf171b06bc12a561699714d9ffe7d03c
ross39 <Rheaney26@gmail.com>
2020-02-11 19:06:10 +0000

    Update MD5.C
    Additional update

--- MD5.C
+++ MD5.C
@@ -5,3 +5,24 @@
  
 // leftrotate function definition
 #define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
+//Lets define some file stuff for dealing with files 
+char ch, file_name[25];
+FILE *fp;
+ 
+// These vars will contain the hash
+uint32_t h0, h1, h2, h3;
+ 
+void md5(uint8_t *initial_msg, size_t initial_len) {
+ 
+    // Message (to prepare)
+    uint8_t *msg = NULL;
+ 
+    // Note: All variables are unsigned 32 bit and wrap modulo 2^32 when calculating
+ 
+    // r specifies the per-round shift amounts
+ 
+    uint32_t r[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+                    5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+                    4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+                    6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
+
